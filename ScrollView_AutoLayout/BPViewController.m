3001deb4d8aa9e17a1bfa3508e7348141dc03ea3
Justin C. Beck <justinbeck@mac.com>
2014-05-20 15:40:07 -0400

    remember timing of autolayout

--- ScrollView_AutoLayout/BPViewController.m
+++ ScrollView_AutoLayout/BPViewController.m
@@ -9,6 +9,9 @@
 #import "BPViewController.h"
 
 @interface BPViewController ()
+{
+    UIView *_longView;
+}
 
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 
@@ -20,16 +23,22 @@
 {
     [super viewDidLoad];
     
-    UIView *longView = [[[NSBundle mainBundle] loadNibNamed:@"BPEmbeddedView" owner:nil options:nil] objectAtIndex:0];
+    _longView = [[[NSBundle mainBundle] loadNibNamed:@"BPEmbeddedView" owner:nil options:nil] objectAtIndex:0];
     
-    [[longView layer] setBorderColor:[UIColor redColor].CGColor];
-    [[longView layer] setBorderWidth:1.0];
+    [[_longView layer] setBorderColor:[UIColor redColor].CGColor];
+    [[_longView layer] setBorderWidth:1.0];
     
-    [[self scrollView] addSubview:longView];
-    [[self scrollView] setContentSize:CGSizeMake(longView.frame.size.width, longView.frame.size.height)];
+    [[self scrollView] addSubview:_longView];
     
     [[[self scrollView] layer] setBorderColor:[UIColor orangeColor].CGColor];
     [[[self scrollView] layer] setBorderWidth:1.0];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    
+    [[self scrollView] setContentSize:CGSizeMake(_longView.frame.size.width, _longView.frame.size.height)];
 }
 
 - (void)didReceiveMemoryWarning
